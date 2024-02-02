ccf9534ebcccd8b6441abfef30436790155bec7b
aman <amannema34@gmail.com>
2024-02-02 10:21:52 +0530

    added changes

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/api/get',(req,res) => {
     res.send({message:'Hello world'})
 })
 
+app.get('/api/get-user',(req,res) => {
+    res.send({message:'Hello user'})
+})
+
 app.listen(5000, () => {
   console.log(`Server running on port 5000`);
 });
